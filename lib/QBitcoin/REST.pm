ae453f94f762ebb6bafd806e8457cb76ae5faaff
QBitcoin Developer 1 <qbitcoin.dev@gmail.com>
2025-09-07 12:57:44 +0200

    Move get_address_txo() to QBitcoin::Utils

--- lib/QBitcoin/REST.pm
+++ lib/QBitcoin/REST.pm
@@ -10,17 +10,14 @@ use Time::HiRes;
 use List::Util qw(sum0);
 use HTTP::Headers;
 use HTTP::Response;
-use Tie::IxHash;
 use QBitcoin::Const;
 use QBitcoin::Config;
 use QBitcoin::Log;
 use QBitcoin::ORM qw(dbh);
-use QBitcoin::Accessors qw(mk_accessors);
-use QBitcoin::Address qw(address_by_hash scripthash_by_address);
-use QBitcoin::RedeemScript;
+use QBitcoin::Address qw(address_by_hash);
 use QBitcoin::Transaction;
-use QBitcoin::TXO;
 use QBitcoin::Block;
+use QBitcoin::Utils qw(get_address_txo);
 use parent qw(QBitcoin::HTTP);
 
 use constant {
@@ -378,54 +375,6 @@ sub txo_stats {
         spent_txo_count  => scalar(grep { defined($_) && defined($_->[0]) } map { @$_ } values %$txo),
         spent_txo_sum    => sum0(map { $_->[1] } grep { defined($_) && defined($_->[0]) } map { @$_ } values %$txo),
     };
-}
-
-sub get_address_txo {
-    my ($address) = @_;
-    my $scripthash = eval { scripthash_by_address($address) }
-        or return ();
-    my %txo_chain;
-    tie %txo_chain, "Tie::IxHash"; # preserve order of keys
-    if (my $script = QBitcoin::RedeemScript->find(hash => $scripthash)) {
-        foreach my $txo (dbh->selectall_array("SELECT tx.hash, num, tx_out, value FROM `" . QBitcoin::TXO->TABLE . "` JOIN `" . QBitcoin::Transaction->TABLE . "` tx ON (tx_in = tx.id) WHERE scripthash = ? ORDER BY block_height ASC, block_pos ASC", undef, $script->id)) {
-            $txo_chain{$txo->[0]}->[$txo->[1]] = [ $txo->[2], $txo->[3] ];
-        }
-    }
-    for (my $height = QBitcoin::Block->max_db_height + 1; $height <= QBitcoin::Block->blockchain_height; $height++) {
-        my $block = QBitcoin::Block->best_block($height)
-            or next;
-        foreach my $tx (@{$block->transactions}) {
-            foreach my $in (@{$tx->in}) {
-                next if $in->{txo}->scripthash ne $scripthash;
-                $txo_chain{$in->{txo}->tx_in}->[$in->{txo}->num]->[0] = $tx->hash;
-            }
-            for (my $num = 0; $num < @{$tx->out}; $num++) {
-                my $out = $tx->out->[$num];
-                next if $out->scripthash ne $scripthash;
-                $txo_chain{$tx->hash}->[$num] = [ undef, $out->value ];
-            }
-        }
-    }
-    my %txo_mempool;
-    tie %txo_mempool, "Tie::IxHash";
-    foreach my $tx (QBitcoin::Transaction->mempool_list()) {
-        foreach my $in (@{$tx->in}) {
-            next if $in->{txo}->scripthash ne $scripthash;
-            if ($txo_mempool{$in->{txo}->tx_in}) {
-                $txo_mempool{$in->{txo}->tx_in}->[$in->{txo}->num]->[0] = $tx->hash;
-            }
-            elsif ($txo_chain{$in->{txo}->tx_in}) {
-                # Unconfirmed spent display as spent
-                $txo_chain{$in->{txo}->tx_in}->[$in->{txo}->num]->[0] = $tx->hash;
-            }
-        }
-        for (my $num = 0; $num < @{$tx->out}; $num++) {
-            my $out = $tx->out->[$num];
-            next if $out->scripthash ne $scripthash;
-            $txo_mempool{$tx->hash}->[$num] = [ undef, $out->value ];
-        }
-    }
-    return (\%txo_chain, \%txo_mempool);
 }
 
 sub get_address_stats {
