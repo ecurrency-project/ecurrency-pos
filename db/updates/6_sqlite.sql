4cf334f59029b5dad965f50895e7e2b7ae564248
QBitcoin Developer 1 <qbitcoin.dev@gmail.com>
2026-03-05 15:08:34 +0100

    db migration for ECR tracking

---
+++ db/updates/6_sqlite.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `my_address_new` (
+  address     varchar(255) NOT NULL PRIMARY KEY,
+  private_key blob(4096)   DEFAULT NULL,
+  staked      int unsigned NOT NULL DEFAULT 0
+);
+INSERT INTO `my_address_new` SELECT * FROM `my_address`;
+DROP TABLE `my_address`;
+ALTER TABLE `my_address_new` RENAME TO `my_address`;
