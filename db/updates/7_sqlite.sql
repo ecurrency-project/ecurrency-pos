949300cbec820398eb8793c6d4c619b3cc54f94d
QBitcoin Developer 1 <qbitcoin.dev@gmail.com>
2026-03-06 17:58:30 +0100

    Migrate database to add algo column to my_address table

---
+++ db/updates/7_sqlite.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `my_address_new` (
+  address     varchar(255) NOT NULL PRIMARY KEY,
+  private_key blob(4096)   DEFAULT NULL,
+  algo        int unsigned NOT NULL DEFAULT 1,
+  staked      int unsigned NOT NULL DEFAULT 0
+);
+INSERT INTO `my_address_new` SELECT * FROM `my_address`;
+DROP TABLE `my_address`;
+ALTER TABLE `my_address_new` RENAME TO `my_address`;
