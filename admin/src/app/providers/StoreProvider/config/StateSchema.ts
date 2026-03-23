bca4f0c8a4b2b6832eca94a0c93cdd19b18ea89b
Ruvym <pair-chops@proton.me>
2026-03-23 11:00:22 +0100

    Replace Redux Block implementation with RTK Query, removing unused state slices, reducers, services, and selectors

--- admin/src/app/providers/StoreProvider/config/StateSchema.ts
+++ admin/src/app/providers/StoreProvider/config/StateSchema.ts
@@ -1,20 +1,5 @@
-import type { AxiosInstance } from 'axios'
-
-import type { BlocksSchema } from '@/entities/Block';
-
 import { rtkApi } from '@/shared/api/rtkApi.ts';
 
 export interface StateSchema {
-    blocks: BlocksSchema;
     [rtkApi.reducerPath]: ReturnType<typeof rtkApi.reducer>;
-}
-
-export interface ThunkExtraArg {
-    api: AxiosInstance,
-}
-
-export interface ThunkConfig<T> {
-    extra: ThunkExtraArg
-    rejectValue: T
-    state: StateSchema
 }
--- admin/src/entities/Block/model/selectors/blockSelectors.ts
+++
@@ -1,4 +0,0 @@
-import type { StateSchema } from '@/app/providers/StoreProvider';
-
-export const getBlocksData = (state: StateSchema) => state.blocks?.entities;
-export const getBlocksLoading = (state: StateSchema) => state.blocks?.isLoading;
