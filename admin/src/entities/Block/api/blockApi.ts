fa8600553121a6acfe141fa91587e9e71ddb06ee
Ruvym <pair-chops@proton.me>
2026-04-02 13:54:59 +0200

    Update API endpoint paths and proxy targets to reflect new `/api` prefix and server configuration changes

--- admin/src/entities/Block/api/blockApi.ts
+++ admin/src/entities/Block/api/blockApi.ts
@@ -5,13 +5,13 @@ import type { BlocksStatus, IBlock } from '../model/types/IBlock.ts';
 const blockApi = rtkApi.injectEndpoints({
     endpoints: (build) => ({
         getBlocks: build.query<IBlock[], number | void>({
-            query: (startHeight) => startHeight ? `/blocks/${startHeight}` : '/blocks',
+            query: (startHeight) => startHeight ? `/api/blocks/${startHeight}` : '/api/blocks',
         }),
         getBlock: build.query<IBlock, { id: string }>({
             query: ({ id }) => `/api/block/${id}`,
         }),
         getBlockStatus: build.query<BlocksStatus, { id: string }>({
-            query: ({ id }) => `/block/${id}/status`,
+            query: ({ id }) => `/api/block/${id}/status`,
         }),
     }),
     overrideExisting: false,
