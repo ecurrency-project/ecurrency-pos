78e8d4fa691d4892ca88b52b7bae742f2f9c8e60
Ruvym <pair-chops@proton.me>
2026-04-28 23:26:07 +0200

    Add ECR brand

--- admin/src/brand/index.ts
+++ admin/src/brand/index.ts
@@ -8,15 +8,12 @@ export interface BrandConfig {
     addressTestnetRe: RegExp;
 }
 
-// Generic base58 address regex as a stub.
-const BASE58_RE = /^[1-9A-HJ-NP-Za-km-z]{20,80}$/;
-
 export const brand: BrandConfig = {
-    assetId: '',
-    assetLabel: 'COIN',
-    assetName: 'Blockchain',
-    addressMainnetRe: BASE58_RE,
-    addressTestnetRe: BASE58_RE,
+    assetId: '6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d',
+    assetLabel: 'ECR',
+    assetName: 'eCurrency',
+    addressMainnetRe: /^(?:EC[1-9A-HJ-NP-Za-km-z]{33}|26[k-n][1-9A-HJ-NP-Za-km-z]{49})$/,
+    addressTestnetRe: /^(?:Et[1-9A-HJ-NP-Za-km-z]{33}|2A[678][1-9A-HJ-NP-Za-km-z]{49})$/,
 };
 
 export { Logo };
