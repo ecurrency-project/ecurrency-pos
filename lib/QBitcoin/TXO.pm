5d85018ef0be55f282f391ebb8aa41e3a9f4d44c
QBitcoin Developer 1 <qbitcoin.dev@gmail.com>
2026-08-01 09:28:34 +0200

    Fix warning in new_txo() when value is undefined

--- lib/QBitcoin/TXO.pm
+++ lib/QBitcoin/TXO.pm
@@ -80,7 +80,8 @@ sub new_txo {
     my $class = shift;
     my $hash = @_ == 1 ? $_[0] : { @_ };
     $hash->{data} //= "";
-    $hash->{value} = int($hash->{value});
+    # Input placeholders (slashing proof verification, signrawtransaction) have no value
+    $hash->{value} = int($hash->{value}) if defined $hash->{value};
     return bless $hash, $class;
 }
 
